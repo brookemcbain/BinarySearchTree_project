9f4b1f8f177782439b3d5e5aa573f87864bd67b7
Brooke McBain <mcbainbrooke@gmail.com>
2020-10-08 08:32:42 -0500

    Binary Search Tree Project

---
+++ BinarySearchTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BinarySearchTree
+    {
+        public Nodes root;
+
+        public BinarySearchTree()
+        {
+            root = null; 
+        }
+
+
+        public void Insert (Nodes newNode)
+        {
+            if (root == null)
+            {
+                root = newNode;
+
+            }
+            else
+            {
+                Nodes CurrentNode = root;
+                if(newNode.data > CurrentNode.data)
+                {
+                    if(CurrentNode.right == null)
+                    {
+                        CurrentNode.right = newNode;
+                    }
+                    else
+                    {
+                        CurrentNode = CurrentNode.right;
+                    }
+                    
+                }
+                else if (newNode.data < CurrentNode.data)
+                {
+                    if(CurrentNode.left == null)
+                    {
+                        CurrentNode.left = newNode; 
+                    }
+                    else
+                    {
+                        CurrentNode = CurrentNode.left; 
+                    }
+                }
+            }
+            
+        }
+        public bool Search(Nodes nodes, int search)
+        {
+
+            if (nodes == null)
+            {
+                return false;
+            }
+            else if (search < nodes.data)
+            {
+                return Search(nodes.left, search);
+
+            }
+            else if (search > nodes.data)
+            {
+                return Search(nodes.right, search);
+            }
+            else return true;  
+        }
+        
+    }
+}
